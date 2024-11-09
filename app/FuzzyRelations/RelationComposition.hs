a4d36657f4fbad61d4bb33b8d75cc14218d369c0
Lukas Balog <lukasbalog66@gmail.com>
2024-11-09 14:52:38 +0100

    fuzzy relations and composition

--- app/FuzzyRelations/RelationComposition.hs
+++ app/FuzzyRelations/RelationComposition.hs
@@ -1,1 +1,26 @@
-module FuzzyRelations.RelationComposition where
+module FuzzyRelations.RelationComposition (
+    circlet,
+    subproduct,
+    superproduct, 
+    square,
+) where
+
+import FuzzyRelations.FuzzyRelation (FuzzyRelation (FuzzyRelation))
+import Lattices.CompleteResiduatedLattice 
+
+
+circlet :: (CompleteResiduatedLattice l) =>  FuzzyRelation a l -> FuzzyRelation a l -> FuzzyRelation a l
+circlet = composition bot (\/) tnorm
+
+subproduct :: (CompleteResiduatedLattice l) => FuzzyRelation a l -> FuzzyRelation a l -> FuzzyRelation a l
+subproduct = composition top (/\) (-->)
+
+superproduct :: (CompleteResiduatedLattice l) => FuzzyRelation a l -> FuzzyRelation a l -> FuzzyRelation a l
+superproduct = composition top (/\) (<--)
+
+square :: (CompleteResiduatedLattice l) => FuzzyRelation a l -> FuzzyRelation a l -> FuzzyRelation a l
+square = composition top (/\) (<-->)
+
+composition :: (CompleteResiduatedLattice l) => l -> (l -> l -> l) -> (l -> l -> l) -> FuzzyRelation a l -> FuzzyRelation a l -> FuzzyRelation a l
+composition unit latOp op (FuzzyRelation r universe) (FuzzyRelation s _) = FuzzyRelation composed universe
+    where composed x z = foldr latOp unit [r x y `op` s y z | y <- universe]
--- app/FuzzySets/FuzzySet.hs
+++ app/FuzzySets/FuzzySet.hs
@@ -1,35 +1,46 @@
 module FuzzySets.FuzzySet(
     FuzzySet(FuzzySet),
     membership,
-    crop
+    crop,
+    union, 
+    intersection,
+    complement,
+    gradedSubsethood,
+    gradedEquality,
     ) where
 
 import Lattices.UnitInterval
 import Lattices.CompleteResiduatedLattice
 
-newtype (CompleteResiduatedLattice l) => FuzzySet a l = FuzzySet (a -> l)
-  
+data (CompleteResiduatedLattice l) => FuzzySet a l = FuzzySet
+    { membershipFunction :: a -> l
+    , universe :: ![a]
+    }
+
+
 membership :: (CompleteResiduatedLattice l) => FuzzySet a l -> a -> l
-membership (FuzzySet memFun) = memFun
+membership (FuzzySet f _) = f
+
 
 crop :: RealFloat a => a -> a
 crop x = fromInteger (round (x * (10^6))) / (10.0^^6)
 
+
 union :: (RealFloat a, CompleteResiduatedLattice l) => FuzzySet a l -> FuzzySet a l -> FuzzySet a l
-union (FuzzySet f) (FuzzySet g) = FuzzySet (\x -> f x \/ g x)
+union (FuzzySet f u) (FuzzySet g _) = FuzzySet (\x -> f x \/ g x) u
 
 intersection :: (RealFloat a, CompleteResiduatedLattice l) => FuzzySet a l -> FuzzySet a l -> FuzzySet a l
-intersection (FuzzySet f) (FuzzySet g) = FuzzySet (\x ->  f x /\ g x)
+intersection (FuzzySet f u) (FuzzySet g _) = FuzzySet (\x ->  f x /\ g x) u
 
 complement :: (RealFloat a, CompleteResiduatedLattice l, Num l) => FuzzySet a l -> FuzzySet a l
-complement (FuzzySet f) = FuzzySet (\x -> 1 - f x)
+complement (FuzzySet f u) = FuzzySet (\x -> 1 - f x) u
 
-gradedSubsethood :: CompleteResiduatedLattice l => [a] -> FuzzySet a l -> FuzzySet a l -> l
+gradedSubsethood :: CompleteResiduatedLattice l => FuzzySet a l -> FuzzySet a l -> l
 gradedSubsethood = gradedOperation (-->)
 
-gradedEqality :: CompleteResiduatedLattice l => [a] -> FuzzySet a l -> FuzzySet a l -> l
-gradedEqality = gradedOperation (<-->)
+gradedEquality :: CompleteResiduatedLattice l => FuzzySet a l -> FuzzySet a l -> l
+gradedEquality = gradedOperation (<-->)
 
-gradedOperation :: CompleteResiduatedLattice l => (l -> l -> l) -> [a] -> FuzzySet a l -> FuzzySet a l -> l
-gradedOperation op interval (FuzzySet f) (FuzzySet g) = 
-    foldr (/\) bot $ zipWith op (map f interval) (map g interval)
+gradedOperation :: CompleteResiduatedLattice l => (l -> l -> l) -> FuzzySet a l -> FuzzySet a l -> l
+gradedOperation op (FuzzySet f u) (FuzzySet g _) = 
+    foldr (/\) bot $ zipWith op (map f u) (map g u)
